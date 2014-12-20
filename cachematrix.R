9ed59e30d7b46f9369854828cccc17168204e6e7
Alexander Papanastassiou <ap@businessquests.com>
2014-12-20 15:10:01 +0100

    Code commented and improved
    This code contains detailed comments of what the two functions do and
    improves the cacheSolve function by showing the user a message stating
    whether the inverse matrix comes from cache or was just computed.

--- cachematrix.R
+++ cachematrix.R
@@ -8,15 +8,27 @@
 ## to get and set values for the matrix and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-         mm<-NULL
-         set <- function(y) {
+        ## setting the "container" of the inverse to value NULL each time the 
+        ## code updating the matrix is run
+        mm<-NULL   
+
+        ## defining the method for setting/updating the matrix to be inverted
+        ## nb: sets the mm variable to null so next time cacheSolve is called
+        ## it knows the computation of matrix inverse needs to run again
+        set <- function(y) {
                  x <<- y
                  mm <<- NULL
          }
-         get <- function() x
-         setinv <- function(solve) mm <<- solve
-         getinv <- function() mm
-         list(set = set, get = get,
+
+        ## defining functions for getting the value of the original matrix,
+        ## computing its inverse and getting its inverse from cache
+        get <- function() x
+        setinv <- function(solve) mm <<- solve
+        getinv <- function() mm
+
+        ## listing all methods that can be accessed by a calling function
+        ## (in this case cacheSolve)
+        list(set = set, get = get,
               setinv = setinv,
               getinv = getinv)
  }
@@ -29,14 +41,27 @@ makeCacheMatrix <- function(x = matrix()) {
 ## that is in cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## Get from cache the value of the inverse matrix
         mm <- x$getinv()
+        
+        ## if that value is **not** NULL, it means the original matrix
+        ## has remained the same, so all we have to do is use the 
+        ## value we just assigned to the mm variable by using the
+        ## method of makeCacheMatrix that allows this function to get the
+        ## inverse matrix from cache
         if(!is.null(mm)) {
                 message("getting cached data")
-                return(mm)
+                return(mm) ## stops execution and returns the cached inverse
         }
+        
+        ## The following lines are executed only if the cached inverse was
+        ## found to be NULL, so we need to s compute the inverse of the original
+        ## matrix by calling from cache the original matrix and using the method
+        ## of makeCacheMatrix that allows us to store the inverse in cache
+        
         data <- x$get()
         mm <- solve(data)
         x$setinv(mm)
-        mm
+        message("inverse matrix just computed for you")
+        mm #this returns the inverse of the original matrix
 }
